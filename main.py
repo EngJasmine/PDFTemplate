89e314fbde8d641ffcd104bb8a7d523fe48f1be1
Yasmine-Nasri <yasmine_nasri@yahoo.fr>
2024-11-05 13:04:25 -0500

    Add footers

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ df=pd.read_csv('topics.csv')
 #unit='mm' means all dimensions will be in millimeters
 
 pdf=FPDF(orientation='P', unit='mm', format='A4')
-for index,item in df.iterrows():
+pdf.set_auto_page_break(auto=False,margin=0)
 
+for index,item in df.iterrows():
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100,100,100)
-    # ln=1 --> ln is actually a break of line --> Go to next line
     pdf.cell(w=0, h=12, txt=item[1], align='L')
+
     pdf.line(10,21,200,21)
+
+    pdf.ln(270)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=item[1], align='R')
+
     for i in range(item['Pages']-1):
     #for i in range(item[2]-1):
         pdf.add_page()
+    pdf.ln(270)
+    pdf.set_font(family='Times', style='I', size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=item[1], align='R')
 pdf.output('output.pdf')
